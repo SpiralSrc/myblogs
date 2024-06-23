3b8cafd2642909d472d49c6e922f266e2e9b1a73
Wyen GW <spiral@Spiralrae.local>
2024-06-22 18:24:12 -0700

    some edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="container">
-      <div className="wrapper">
-        <h1>Homepage</h1>
-      </div>
+    <div className="wrapper">
+      <h1>Homepage</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xxs: "350px",
+        xs: "480px",
+        sm: "640px",
+        md: "840px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1620px",
+        "4xl": "1920px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
